aa7da81a873d633ab1182bfc59432cc83a650cc1
ediana-ekanem <edianaekanem@gmail.com>
2024-07-16 21:28:29 +0100

    new changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../container/Container";
 import logo from "/assets/icons/logo.svg";
-import Searchbar from "../searchbar/SearchBar";
+import SearchBar from "../searchbar/SearchBar";
 import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
-  const handleSearch = (query) => {
-    console.log("Searching for:", query);
-    // Add your search logic here
-  };
-
   return (
-    <div className="bg-[#002147] text-white py-10 font-Roboto">
+    <div className="bg-cyan-700 text-white py-10 font-Roboto">
       <Container>
         <nav className="flex gap-20  text-lg items-center justify-between">
           <div className="flex gap-5 items-center">
@@ -22,7 +17,7 @@ const Navbar = () => {
               </Link>
             </div>
             <div>
-              <Searchbar onsearch={handleSearch} />
+              <SearchBar />
             </div>
           </div>
           <ul className="flex gap-5 items-center">
--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,35 +1,25 @@
 import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
-  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSearch(query);
+  const handleIconClick = () => {
+    setShowSearch(!showSearch);
   };
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        className="flex items-center justify-center"
-      >
-        <input
-          type="text"
-          className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Search..."
-          value={query}
-          onChange={handleChange}
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 ml-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Search
-        </button>
+      <form className="flex items-center justify-center ">
+        <CiSearch size={25} onClick={handleIconClick} />
+        {showSearch && (
+          <div>
+            <input
+              type="text"
+              className="border-0 border-b bg-transparent border-white focus:outline-none focus:ring-0 focus:border-white"
+              placeholder=""
+            />
+          </div>
+        )}
       </form>
     </>
   );
